fc2bd51264c1d4a0601fa0f7d4c0965267553672
Artyomkin141z <73499508+Artyomkin141z@users.noreply.github.com>
2023-05-13 13:19:37 +0300

    2

--- client/src/components/global/header.jsx
+++ client/src/components/global/header.jsx
@@ -1,31 +1,53 @@
+'use client'
 import styles from '../../styles/components/global/header.module.scss'
-import logo from '../../../public/image/global/small-logo.svg'
+import logo from '../../../public/image/global/logo.png'
 
 import Link from "next/link";
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const handleScroll = () => {
+        const position = window.pageYOffset;
+        setScrollPosition(position);
+    };
+
+    const router = usePathname();
+
     return (
         <div className={styles.header}>
             <div className={styles.top}>
                 <h4>Новополоцкий хлебозавод, Филиал <Link href='http://vhp.by/'>{'ОАО "Витебскхлебпром"'}</Link> </h4>
                 <div>
                     <h4>Контакты</h4>
-                    <h4><Link href=''>Ру</Link></h4>
-                    <h4><Link href=''>Бел</Link></h4>
+                    <h4>Награды</h4>
+                    <h4>Вакансии</h4>
                 </div>
             </div>
-            <header>
-                <Link href='/' className={styles.logo}>
-                    <Image className={styles.img} src={logo} alt='Полоцкий хлебозавод'/>
-                    <h1 className={styles.logoName}>Новополоцкий хлебозавод</h1>
-                </Link>
-                <nav>
-                    <Link href='/' >О нас</Link>
-                    <Link href='/' >Продукция</Link>
-                    <Link href='/' >Новости</Link>
-                </nav>
-            </header>
+            <div className={(scrollPosition > 40 || router.pathname === '/') ? styles.scrolled : ''}>
+                <header>
+                    <Link href='/' className={styles.logo}>
+                        <Image className={styles.img} src={logo} alt='Полоцкий хлебозавод' />
+                        <h1 className={styles.logoName}>Новополоцкий хлебозавод</h1>
+                    </Link>
+                    <nav>
+                        <Link href='/' >О нас</Link>
+                        <Link href='/' >Продукция</Link>
+                        <Link href='/' >Новости</Link>
+                    </nav>
+                </header>
+            </div>
         </div>
     )
 }
